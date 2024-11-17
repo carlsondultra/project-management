cdc33fbaf71c20abca15759a6ffab5bcaccc0009
carlsondultra <carlsondultra@users.noreply.github.com>
2024-11-16 23:13:50 -0500

    creating sidebar skeleton

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React, { useState } from 'react'
+
+const Sidebar = () => {
+    const [showProjects, setShowProjects] = useState(true)
+
+  return (
+    <div>Sidebar Skeleton</div>
+  )
+}
+
+export default Sidebar
