7ce68c6f167b6581ae6ee33eeef20057f04b902a
carlsondultra <carlsondultra@users.noreply.github.com>
2024-12-02 22:19:24 -0500

    creating project header

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Header from '@/components/Header'
+import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+  activeTab: string
+  setActiveTab: (tabName: string) => void
+}
 
-const ProjectHeader = (props: Props) => {
-  return <div>ProjectHeader</div>
+const ProjectHeader = (activeTab, setActiveTab: Props) => {
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false)
+
+  return <div className="px-4 xl:px-6">
+    <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+      <Header name="Product Design Development" />
+    </div>
+    </div>
 }
 
 export default ProjectHeader
