24618a64e4cb8f9f968926bdd27efbcf56a9e1da
carlsondultra <carlsondultra@users.noreply.github.com>
2024-12-08 23:06:46 -0500

    adding loading and error properties to boardView

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -1,4 +1,4 @@
-import { useGetTasksQuery } from "@/state/api";
+import { useGetTasksQuery, useUpdateTaskStatusMutation } from "@/state/api";
 import React from "react";
 
 type BoardProps = {
@@ -14,6 +14,14 @@ const BoardView = ({ id, setIsModalNewTaskOpen }: BoardProps) => {
     isLoading,
     error,
   } = useGetTasksQuery({ projectId: Number(id) });
+  const [updateTaskStatus] = useUpdateTaskStatusMutation()
+
+  const moveTask = (taskId: number, toStatus: string) => {
+    updateTaskStatus({ taskId, status: toStatus })
+  }
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>An error occurred while fetching tasks</div>
 
   return <div>BoardView</div>;
 };
