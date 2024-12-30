a63ff368e46312806b6a8ec5a782d6b693c4d881
carlsondultra <carlsondultra@users.noreply.github.com>
2024-12-30 16:37:06 -0500

    redirecting images to s3

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "p-management-s3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
