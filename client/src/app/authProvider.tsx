e64fa915563c063518f5e8a14a19a81c2564cd9f
carlsondultra <carlsondultra@users.noreply.github.com>
2025-01-03 21:20:49 -0500

    wrapping dashboard with aws cognito

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-type Props = {}
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+    },
+  },
+});
 
-const AuthProvider = ({children}: any) => {
+const AuthProvider = ({ children }: any) => {
   return (
-    <div>AuthProvider</div>
-  )
-}
+    <div className="mt-5">
+      <Authenticator>
+        {({ user }: any) =>
+          user ? (
+            <div>{children}</div>
+          ) : (
+            <div>
+              <h1>Please sign in below:</h1>
+            </div>
+          )
+        }
+      </Authenticator>
+    </div>
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
