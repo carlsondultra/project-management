e04b438dcbac9e07dd196b13a4b8a5c7b269a88d
carlsondultra <carlsondultra@users.noreply.github.com>
2025-01-05 22:40:37 -0500

    adding comments

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -23,14 +23,14 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* sidebar */}
+      {/* sidebar component */}
       <Sidebar />
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
           isSidebarCollapsed ? "" : "md:pl-64"
         }`}
       >
-        {/* navbar */}
+        {/* navbar component */}
         <Navbar />
         {children}
       </main>
