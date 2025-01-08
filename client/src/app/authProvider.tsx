586ac6ab14fffb449b815aa2821bfa3fd4817184
carlsondultra <carlsondultra@users.noreply.github.com>
2025-01-07 22:54:55 -0500

    removing un-needed import

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css"
-import { signUp } from "aws-amplify/auth";
 
 Amplify.configure({
   Auth: {
